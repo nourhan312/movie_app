d85d2ff195127e57e5a3718483200d9b097cbd25
SaifSamir1 <saifsamirsaif12@gmail.com>
2024-08-01 14:10:31 +0300

    finish core file branch

---
+++ lib/core/theming/text_style.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'font_weight_helper.dart';
+
+class TextStyles {
+  static TextStyle font24Bold = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeightHelper.bold,
+  );
+
+  static TextStyle font32Bold = TextStyle(
+    fontSize: 32.sp,
+    fontWeight: FontWeightHelper.bold,
+  );
+
+  static TextStyle font13SemiBold = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: FontWeightHelper.semiBold,
+  );
+
+  static TextStyle font13Medium = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: FontWeightHelper.medium,
+  );
+
+  static TextStyle font13Regular = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: FontWeightHelper.regular,
+  );
+
+  static TextStyle font24BoldAlt = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeightHelper.bold,
+  );
+
+  static TextStyle font16SemiBold = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeightHelper.semiBold,
+  );
+
+  static TextStyle font13RegularAlt = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: FontWeightHelper.regular,
+  );
+
+  static TextStyle font12Regular = TextStyle(
+    fontSize: 12.sp,
+    fontWeight: FontWeightHelper.regular,
+  );
+
+  static TextStyle font12Medium = TextStyle(
+    fontSize: 12.sp,
+    fontWeight: FontWeightHelper.medium,
+  );
+
+  static TextStyle font12RegularAlt = TextStyle(
+    fontSize: 12.sp,
+    fontWeight: FontWeightHelper.regular,
+  );
+
+  static TextStyle font12RegularAlt2 = TextStyle(
+    fontSize: 12.sp,
+    fontWeight: FontWeightHelper.regular,
+  );
+
+  static TextStyle font13RegularAlt2 = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: FontWeightHelper.regular,
+  );
+
+  static TextStyle font14Regular = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeightHelper.regular,
+  );
+
+  static TextStyle font14RegularAlt = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeightHelper.regular,
+  );
+
+  static TextStyle font14Medium = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeightHelper.medium,
+  );
+
+  static TextStyle font14Bold = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeightHelper.bold,
+  );
+
+  static TextStyle font16Medium = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeightHelper.medium,
+  );
+
+  static TextStyle font14SemiBold = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeightHelper.semiBold,
+  );
+
+  static TextStyle font15Medium = TextStyle(
+    fontSize: 15.sp,
+    fontWeight: FontWeightHelper.medium,
+  );
+
+  static TextStyle font18Bold = TextStyle(
+    fontSize: 18.sp,
+    fontWeight: FontWeightHelper.bold,
+  );
+
+  static TextStyle font18SemiBold = TextStyle(
+    fontSize: 18.sp,
+    fontWeight: FontWeightHelper.semiBold,
+  );
+
+  static TextStyle font48SemiBold = TextStyle(
+    fontSize: 48.sp,
+    fontWeight: FontWeightHelper.medium,
+  );
+}
