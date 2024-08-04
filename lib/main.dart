fa24d4eaa693f18cf8070fbd73e499ae47dfcec4
SaifSamir1 <saifsamirsaif12@gmail.com>
2024-08-05 02:54:31 +0300

    -------

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -6,17 +5,6 @@ import 'package:hive_flutter/adapters.dart';
 import 'package:movie_app/core/helper/hive_helper.dart';
 import 'package:movie_app/core/networking/services/search_movie.dart';
 import 'core/networking/dio_helper.dart';
-import 'package:movie_app/features/details/ui/detail_screen.dart';
-import 'package:movie_app/core/networking/services/get_movie_details.dart';
-import 'package:movie_app/core/networking/services/search_movie.dart';
-import 'package:movie_app/features/home/data/models/credits_model.dart';
-import 'package:movie_app/features/home/data/models/details_model.dart';
-import 'package:movie_app/features/home_screen/ui/home_screen.dart';
-import 'package:movie_app/features/search/ui/search.dart';
-import 'core/networking/dio_helper.dart';
-import 'core/networking/services/get_credits.dart';
-import 'core/networking/services/get_similar_movie.dart';
-import 'core/networking/services/get_top_rated.dart';
 import 'core/routing/app_router.dart';
 import 'core/routing/routes.dart';
 import 'features/home_screen/data/models/movie_model.dart';
@@ -32,25 +20,11 @@ void main() async {
   await Hive.openBox<Movie>(HiveHelpers.movieBox);
   DioHelper.init();
 
-  List<Movie> result =
-      await SearchMovie.searchMoviesByQuery(query: 'Inception');
-  for (var i in result) {
-    if (kDebugMode) {
-      print(i.originalTitle);
-    }
-  Credits result = await GetCredits.getCredits(
-    seriesId: 1399,
-    seasonNumber: 1,
-    episodeNumber: 1,
-  );
-  List<Cast> cast = result.cast;
-  for (var element in cast) {
-    print(element.name);
+  await SearchMovie.searchMoviesByQuery(query: 'Inception');
+    runApp(MyApp(
+      appRouter: AppRouter(),
+    ));
   }
-  runApp(MyApp(
-    appRouter: AppRouter(),
-  ));
-}
 
 class MyApp extends StatelessWidget {
   final AppRouter appRouter;
