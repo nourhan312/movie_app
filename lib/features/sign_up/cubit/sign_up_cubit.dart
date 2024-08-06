8dc0d3115a9ad427ad5ff536b4130d8c7eba14f0
Mina Samir <menasamer484@gmail.com>
2024-08-06 19:26:44 +0200

    sign up screen

---
+++ lib/features/sign_up/cubit/sign_up_cubit.dart
@@ -0,0 +1,22 @@
+import 'package:bloc/bloc.dart';
+import 'package:dio/dio.dart';
+import 'package:meta/meta.dart';
+
+import '../../../core/networking/api_endpoints.dart';
+import '../../../core/networking/dio_helper.dart';
+
+part 'sign_up_state.dart';
+
+class SignUpCubit extends Cubit<SignUpState> {
+  SignUpCubit() : super(SignUpInitial());
+  void SignUp(){
+    final response = DioHelper.postData(
+        path: ApiEndPoints.signup,
+      body: {
+
+      }
+
+
+    );
+  }
+}
